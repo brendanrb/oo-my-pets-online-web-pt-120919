89c4e903c15f25db01d243237358188b3dc65b20
Brendan R Borgers <brendanborgers@gmail.com>
2020-02-10 16:16:10 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,20 @@
-class Cat
-  # code goes here
+class Cat 
+  
+  attr_reader :name
+  
+  attr_accessor :mood, :owner
+  
+  @@all = []
+  
+  def initialize(name, owner)
+    @owner = owner
+    @name = name
+    @mood = "nervous"
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,41 @@
 class Owner
-  # code goes here
+  
+  attr_reader :species , :name
+   attr_accessor :human, :pets
+   @@all = []
+  
+  def initialize(name, species = "human")
+    @name = name
+    @species = species
+    @@all << self
+    @pets = {:dogs => [], :cats => []}
+  end
+  
+  def species
+    @species
+  end
+  
+  
+  def say_species
+   return "I am a human."
+  end
+    
+  def self.all
+    return @@all
+  end
+  
+  def self.count
+    return @@all.size 
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats
+    self.cats
+  end
+  
+  
+  
 end
